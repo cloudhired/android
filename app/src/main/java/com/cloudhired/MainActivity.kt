b15dbbe8b453aca2f47311aefa1a156d9ed364bf
gaomengen <gaomengen@gmail.com>
2020-11-14 21:49:34 -0500

    can login with google account

--- app/src/main/java/com/cloudhired/MainActivity.kt
+++ app/src/main/java/com/cloudhired/MainActivity.kt
@@ -3,18 +3,24 @@ package com.cloudhired
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.content.Intent
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.viewModels
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+//    private val viewMdoel: M
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val authInitIntent = Intent(this, AuthInitActivity::class.java)
+        startActivity(authInitIntent)
+
 
         topAppBar.setNavigationOnClickListener {
             println("you clicked topapp bar")
@@ -34,6 +40,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.more -> {
                     // Handle more item (inside overflow menu) press
+                    FirebaseAuth.getInstance().signOut()
                     println("more clicked")
                     true
                 }
@@ -41,4 +48,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
 }
